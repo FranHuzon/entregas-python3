d9669547beb2eb2e8a6687fde8e2ea99e1e5e38e
FranHuzon <huzonvillarfranciscojesus@gmail.com>
2018-02-22 09:39:30 +0100

    He realizado la función convertir_a_euros

--- ejercicio_funciones.py
+++ ejercicio_funciones.py
@@ -8,3 +8,8 @@ def calcular_coste(segundos,precio):
 	total_a_pagar=segundos*precio
 	return total_a_pagar
 
+def convertir_a_euros(centimos):
+
+	euros=centimos/100
+	resto=centimos%100
+	return (euros,resto)
