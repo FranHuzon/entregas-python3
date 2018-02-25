ca69e05d99d9dcb4095d5c9644985c567fb8a823
FranHuzon <huzonvillarfranciscojesus@gmail.com>
2018-02-25 15:28:58 +0100

    He realizado el ejercicio2

--- ejercicio_funciones.py
+++ ejercicio_funciones.py
@@ -53,6 +53,9 @@ print("-"*10,"Ejercicio 2","-"*10)
 # Ejercicio 2 #
 ###############
 
+#He supuesto que la primera linea del fichero indica el precio por segundo
+#de la llamada, en este caso 3 céntimos cada segundo.
+
 print()
 
 with open("comunicaciones.txt","r") as fichero:
@@ -62,7 +65,33 @@ datos2=[]
 for i in datos:
 	datos2.append(i.replace("\n",""))
 
+listatarifa=datos2[0].split(" ")
+tarifa=int(listatarifa[1])
 
+llamadas=[]
 
+for i in datos2[1:]:
+	cadallamada=[]
+	hh,mm,ss=i.split(":")
+	cadallamada.append(int(hh))
+	cadallamada.append(int(mm))
+	cadallamada.append(int(ss))
+	llamadas.append(cadallamada)
 
+sumasegundos=[]
+totalsegundos=0
+
+for i in llamadas:
+	contador=0
+	contador=contador+pasar_a_segundos(i[0],i[1],i[2])
+	sumasegundos.append(contador)
+	totalsegundos=totalsegundos+contador
+
+numeracion=1
+
+for i in sumasegundos:
+	print("El coste de la llamada",numeracion,"es.....",round(convertir_a_euros(float(calcular_coste(i,tarifa))),2),"€")
+	numeracion+=1
+
+print("El coste total de las llamadas es.....",round(convertir_a_euros(calcular_coste(totalsegundos,tarifa)),2),"€")
 
