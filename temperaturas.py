cc27553e23134385eb236e21e28a0b38d8ea6c2b
FranHuzon <huzonvillarfrancisojesus@gmail.com>
2018-02-28 18:24:49 +0100

    Añadida funcion leer_url, sin acabar

--- temperaturas.py
+++ temperaturas.py
@@ -5,11 +5,17 @@ def encontrar_url_municio(municipio):
 	municipios=doc.findall("municipio")
 	for municipio in municipios:
 		num_id=municipio.attrib["value"].split("-")
+		url="http://www.aemet.es/xml/municipios/localidad_"+num_id[1].replace("id","")+".xml"
+		return url
 
-		return "http://www.aemet.es/xml/municipios/localidad_"+num_id[1].replace("id","")+".xml"
+def leer_url(url):
+	import webbrowser
+	web=webbrowser.open(url)
+	return web
+
 
 municipio=input("Indique el municipio de sevilla que desee: ")
 
-print(encontrar_url_municio(municipio))
+print(leer_url(encontrar_url_municio(municipio)))
 
 
