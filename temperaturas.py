833fbd6c188cb2346fabe2da16d3b51dc95b34aa
FranHuzon <huzonvillarfrancisojesus@gmail.com>
2018-03-02 14:01:58 +0100

    Añadido fichero de las funciones de temperaturas.py

--- temperaturas.py
+++ temperaturas.py
@@ -1,34 +1,15 @@
-from lxml import etree
-import time
-def encontrar_url_municio(municipio):
-	
-	doc=etree.parse('id_municipios_sevilla.xml')
-
-	municipios=doc.findall("municipio")
-	for i in municipios:
-		if municipio.upper()==i.text.upper():
-			num_id=i.attrib["value"][-5:]
-			url="http://www.aemet.es/xml/municipios/localidad_"+num_id+".xml"
-			return url
-	return False
-
-
-def leer_url(url):
-	
-	doc=etree.parse(url)
-	prediccion=doc.findall('prediccion/dia')
-	lista=[]
-	for i in prediccion:
-		if i.attrib["fecha"]==time.strftime("%Y-%m-%d"):
-			lista.append(i.find("temperatura/maxima").text)
-			lista.append(i.find("temperatura/minima").text)
-			return lista
-	return False
-
+from funciones_	temperaturas.py import encontrar_url_municio,leer_url
 
 municipio=input("Indique el municipio de sevilla que desee (respetando las tíldes): ")
+
 url=encontrar_url_municio(municipio)
 lista=leer_url(url)
 print("La temperatura máxima para",municipio.upper(),"es",lista[0],"ºC y la mínima de",lista[1],"ºC")
 
 
+if not encontrar_url_municio(municipio):
+#	print("Municipio no encontrado")
+#else:
+#	print(encontrar_url_municio(municipio))
+# 
+#print(leer_url(municipio))
