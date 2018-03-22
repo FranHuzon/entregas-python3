21c71933f3600adf374eb46ade764b8a3cbd4c58
FranHuzon <huzonvillarfrancisojesus@gmail.com>
2018-03-22 09:57:54 +0100

    Realizado primer apartado del ejercicio Zomato

--- ZOMATO/ejercicio_zomato.py
+++ ZOMATO/ejercicio_zomato.py
@@ -1,7 +1,11 @@
 
-curl -X GET --header "Accept: application/json" --header "user-key: 7dc2fc52b46a2d3de6d017123ce59cca" "https://developers.zomato.com/api/v2.1/categories"
-
 headers={}
 
-headers[Accept]="application/json"
-headers[user-key]="7dc2fc52b46a2d3de6d017123ce59cca"
+headers["Accept"]="application/json"
+headers["user-key"]="7dc2fc52b46a2d3de6d017123ce59cca"
+
+r=requests.get("https://developers.zomato.com/api/v2.1/categories",headers=headers)
+if r.status_code==200:
+	import json
+	doc=r.json()
+	print(doc)
