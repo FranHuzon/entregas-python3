03101864e36ce6eff61cd5093b1042362422a430
FranHuzon <huzonvillarfrancisojesus@gmail.com>
2018-03-22 09:52:29 +0100

    añadida carpeta Zomato

---
+++ ZOMATO/ejercicio_zomato.py
@@ -0,0 +1,7 @@
+
+curl -X GET --header "Accept: application/json" --header "user-key: 7dc2fc52b46a2d3de6d017123ce59cca" "https://developers.zomato.com/api/v2.1/categories"
+
+headers={}
+
+headers[Accept]="application/json"
+headers[user-key]="7dc2fc52b46a2d3de6d017123ce59cca"
