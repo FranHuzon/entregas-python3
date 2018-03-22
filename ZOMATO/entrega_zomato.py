958795952407552ff70cea55de814885f1a2d2ba
FranHuzon <huzonvillarfrancisojesus@gmail.com>
2018-03-22 11:27:20 +0100

    Realizada la parte que saca la ID de la ciudad, no completa

--- ZOMATO/entrega_zomato.py
+++ ZOMATO/entrega_zomato.py
@@ -1,2 +1,14 @@
 with open("api_key_zomato","r") as fichero:
 	key=fichero.read().replace("\n","")
+
+import requests
+
+headers={"Accept":"application/json","user-key":key}
+ciudad=input("Dime la ciudad en inglés: ").replace(" ","%20")
+
+r=requests.get("https://developers.zomato.com/api/v2.1/cities?q="+ciudad,headers=headers)
+if r.status_code==200:
+	import json
+	doc=r.json()
+	for i in doc["location_suggestions"]:
+		print(i)
