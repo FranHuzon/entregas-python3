ea149265c4b1e1589ee8c2ee2018a60e70f095d5
FranHuzon <huzonvillarfrancisojesus@gmail.com>
2018-03-22 10:21:09 +0100

    Mejorado elcódigo para que no muestre la api_key

--- ZOMATO/ejercicio_zomato.py
+++ ZOMATO/ejercicio_zomato.py
@@ -1,9 +1,11 @@
-import requests
+with open("api_key_zomato","r") as fichero:
+	key=fichero.read().replace("\n","")
 
+import requests
 headers={}
 
 headers["Accept"]="application/json"
-headers["user-key"]="7dc2fc52b46a2d3de6d017123ce59cca"
+headers["user-key"]=key
 
 r=requests.get("https://developers.zomato.com/api/v2.1/categories",headers=headers)
 if r.status_code==200:
